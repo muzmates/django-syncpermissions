5faa1e8715735b29c0203db85c5b86a1398d1249
Max Kuznetsov <syhpoon@gmail.com>
2016-01-19 22:20:17 -0800

    Fix for Django 1.9

--- django-syncpermissions/management/commands/sync_permissions.py
+++ django-syncpermissions/management/commands/sync_permissions.py
@@ -1,22 +1,20 @@
 # -*- coding: UTF-8 -*-
 from django.core.management.base import BaseCommand
-from django.db.models import get_models, get_app
+from django.apps import apps as a
 from django.contrib.auth.management import create_permissions
-from django.apps import apps
 
 class Command(BaseCommand):
     args = '<app app ...>'
     help = 'Sync permissions for specified apps, or all apps if none specified'
 
     def handle(self, *args, **options):
-        _apps = []
-
         if not args:
-            for model in get_models():
-                _apps.append(apps.get_app_config(model._meta.app_label))
+            apps = []
+            for model in a.get_models():
+                apps.append(a.get_app_config(model._meta.app_label))
         else:
+            apps = []
             for arg in args:
-                _apps.append(apps.get_app_config(arg))
-
-        for app in _apps:
-            create_permissions(app, get_models(), options.get('verbosity', 0))
+                apps.append(a.get_app_config(arg))
+        for app in apps:
+            create_permissions(app, a.get_models(), options.get('verbosity', 0))
